a5f7c152141ba6383a51c6148ba7d432bcf0881b
eepodrezov <egor.podrezov@kirarock.team>
2024-06-08 21:11:36 +0300

    15. Стрелочка заменена на значок лупы и добавлена обработка события в js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('privet')
+function openSearch() {
+    const searchButton = document.querySelector("#search_button")
+    
+    searchButton.addEventListener('click', toogleSearch)
+}
+openSearch()
+
+function toogleSearch() {
+    const searchBlock = document.getElementById('search_block')
+    
+    searchBlock.classList.toggle('hidden')
+}
